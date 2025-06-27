b45d17883b73ab77df255cc63ad4803c87b9adfc
Juhász Zoltán <juhasz.zoltan@freemail.hu>
2025-06-28 01:04:25 +0200

    #3

--- src/discovery.cpp
+++ src/discovery.cpp
@@ -10,7 +10,7 @@ void DiscoveryHandler::begin(const String& device_name) {
     //_device_name += device_name;
     //_device_name += ".local";
     MDNS.begin(_device_name);
-    _udp.listen(5353);
+    _udp.listen(6053);
 }
 void DiscoveryHandler::loop() {
     if (millis() - _last_broadcast_time > BROADCAST_INTERVAL) {
@@ -20,6 +20,5 @@ void DiscoveryHandler::loop() {
 }
 
 void DiscoveryHandler::_sendUDPPacket() {
-    String message = R"({"name":"esphome-dev2v","platform":"esp8266","board":"esp01"})";
-    _udp.broadcastTo(message.c_str(), 5353);
+    _udp.broadcastTo(_device_name.c_str(), 6053);
 }
